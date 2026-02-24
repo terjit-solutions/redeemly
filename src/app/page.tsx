a7e270d5b6cdd7a08acf76338aaaa2d1b2ddf740
isselmou ainina <isselmou.ainina@gmail.com>
2026-02-23 22:10:57 -0500

    Remove Trust section from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { HeroSection } from "@/components/landing/HeroSection";
 import { TrustStrip } from "@/components/landing/TrustStrip";
 import { HowItWorksSection } from "@/components/landing/HowItWorksSection";
 import { PopularServices } from "@/components/landing/PopularServices";
-import { TrustSection } from "@/components/landing/TrustSection";
 
 import { PricingPreview } from "@/components/landing/PricingPreview";
 import { LandingFooter } from "@/components/landing/LandingFooter";
@@ -17,8 +16,6 @@ export default function HomePage() {
       <TrustStrip />
       <HowItWorksSection />
       <PopularServices />
-      <TrustSection />
-
       <PricingPreview />
       <LandingFooter />
       <MobileStickyBar />
